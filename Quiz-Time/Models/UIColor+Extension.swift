5174e105c3ed09d8ebf71b72b04f91244eb26e3e
Melissa He <melissahe@ac.c4q.nyc>
2018-02-19 18:14:47 -0500

    Change transport security

---
+++ Quiz-Time/Models/UIColor+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  UIColor+Extension.swift
+//  Quiz-Time
+//
+//  Created by C4Q on 2/19/18.
+//  Copyright © 2018 Melissa He @ C4Q. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    public static var lightPurple = UIColor(red: 0.72, green: 0.71, blue: 0.98, alpha: 1)
+    public static var buttonBlue = UIColor(red: 0.35, green: 0.71, blue: 0.98, alpha: 1)
+}
