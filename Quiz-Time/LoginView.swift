4401f24b733149e6ce55811be236c0f7786cecc2
Melissa He <melissahe@ac.c4q.nyc>
2018-02-19 18:14:19 -0500

    Set up login view and VC

--- Quiz-Time/LoginView.swift
+++ Quiz-Time/LoginView.swift
@@ -8,9 +8,97 @@
 
 import UIKit
 import SnapKit
+import GoogleSignIn
 
 class LoginView: UIView {
 
+    lazy var appTitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Quiz Time"
+        label.font = UIFont(name: "Chalkduster", size: 48)
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        label.setContentHuggingPriority(UILayoutPriority(249), for: .vertical)
+        label.textColor = UIColor.lightPurple
+        return label
+    }()
+    
+    lazy var emailLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Email:"
+        label.textAlignment = .left
+        label.font = UIFont(name: "Chalkduster", size: 16)
+        label.textColor = UIColor.lightPurple
+        label.setContentHuggingPriority(UILayoutPriority(253), for: .horizontal)
+        label.setContentCompressionResistancePriority(UILayoutPriority(rawValue: 1000), for: .horizontal)
+        return label
+    }()
+    
+    lazy var emailTextField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .roundedRect
+        textField.placeholder = "Email..."
+        return textField
+    }()
+    
+    lazy var passwordLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Password:"
+        label.textAlignment = .left
+        label.font = UIFont(name: "Chalkduster", size: 16)
+        label.textColor = UIColor.lightPurple
+        label.setContentHuggingPriority(UILayoutPriority(253), for: .horizontal)
+        label.setContentCompressionResistancePriority(UILayoutPriority(rawValue: 1000), for: .horizontal)
+        return label
+    }()
+    
+    lazy var passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .roundedRect
+        textField.placeholder = "Password..."
+        textField.isSecureTextEntry = true
+        return textField
+    }()
+    
+    lazy var emailStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [emailLabel, emailTextField])
+        stackView.alignment = .fill
+        stackView.axis = .horizontal
+        stackView.spacing = 5
+        stackView.distribution = .fill
+        return stackView
+    }()
+    
+    lazy var passwordStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [passwordLabel, passwordTextField])
+        stackView.alignment = .fill
+        stackView.axis = .horizontal
+        stackView.spacing = 5
+        stackView.distribution = .fill
+        return stackView
+    }()
+    
+    lazy var loginButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Login", for: .normal)
+        button.setTitleColor(UIColor.buttonBlue, for: .normal)
+        return button
+    }()
+    
+    lazy var createButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Create Account", for: .normal)
+        button.setTitleColor(UIColor.buttonBlue, for: .normal)
+        return button
+    }()
+    
+    lazy var googleSignInButton: GIDSignInButton = {
+        let button = GIDSignInButton()
+        button.colorScheme = .light
+        button.style = GIDSignInButtonStyle.wide
+        return button
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: UIScreen.main.bounds)
         commonInit()
@@ -27,7 +115,77 @@ class LoginView: UIView {
     }
     
     private func setUpViews() {
-        
+        setUpAppTitleLabel()
+        setUpEmailStackView()
+        setUpPasswordStackView()
+        setUpLoginButton()
+        setUpCreateButton()
+        setUpGoogleSignInButton()
     }
-
+    
+    private func setUpAppTitleLabel() {
+        self.addSubview(appTitleLabel)
+        
+        appTitleLabel.snp.makeConstraints { (make) in
+            make.top.leading.trailing.equalTo(self).inset(80)
+        }
+    }
+    
+    private func setUpEmailStackView() {
+        self.addSubview(emailStackView)
+        
+        emailStackView.snp.makeConstraints { (make) in
+            make.top.equalTo(appTitleLabel.snp.bottom).offset(80)
+            make.leading.trailing.equalTo(self).inset(50)
+        }
+    }
+    
+    private func setUpPasswordStackView() {
+        self.addSubview(passwordStackView)
+        
+        passwordStackView.snp.makeConstraints { (make) in
+            make.top.equalTo(emailStackView.snp.bottom).offset(10)
+            make.width.equalTo(emailStackView.snp.width)
+            make.centerX.equalTo(emailStackView.snp.centerX)
+        }
+    }
+    
+    private func setUpLoginButton() {
+        self.addSubview(loginButton)
+        
+        loginButton.snp.makeConstraints { (make) in
+            make.top.equalTo(passwordStackView.snp.bottom).offset(40)
+            make.centerX.equalTo(passwordStackView.snp.centerX)
+            make.width.equalTo(self.snp.width).multipliedBy(0.20)
+        }
+        
+        loginButton.layer.borderColor = UIColor.lightPurple.cgColor
+        loginButton.layer.borderWidth = 1.0
+        loginButton.layer.masksToBounds = true
+        loginButton.layer.cornerRadius = 10
+    }
+    
+    private func setUpCreateButton() {
+        self.addSubview(createButton)
+        
+        createButton.snp.makeConstraints { (make) in
+            make.top.equalTo(loginButton.snp.bottom).offset(10)
+            make.centerX.equalTo(loginButton.snp.centerX)
+            make.width.equalTo(self.snp.width).multipliedBy(0.40)
+        }
+        
+        createButton.layer.borderColor = UIColor.lightPurple.cgColor
+        createButton.layer.borderWidth = 1.0
+        createButton.layer.masksToBounds = true
+        createButton.layer.cornerRadius = 10
+    }
+    
+    private func setUpGoogleSignInButton() {
+        self.addSubview(googleSignInButton)
+        
+        googleSignInButton.snp.makeConstraints { (make) in
+            make.centerX.equalTo(passwordStackView.snp.centerX)
+            make.bottom.equalTo(self.snp.bottom).offset(-40)
+        }
+    }
 }
--- Quiz-Time/View Controllers/LoginVC.swift
+++ Quiz-Time/View Controllers/LoginVC.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import GoogleSignIn
+import FirebaseAuth
 
 class LoginVC: UIViewController {
 
@@ -14,11 +16,69 @@ class LoginVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        GIDSignIn.sharedInstance().uiDelegate = self
+        GIDSignIn.sharedInstance().signInSilently()
         setUpViews()
     }
 
     private func setUpViews() {
         self.view.addSubview(loginView)
+        
+        loginView.loginButton.addTarget(self, action: #selector(loginButtonPressed), for: .touchUpInside)
+        loginView.createButton.addTarget(self, action: #selector(createButtonPressed), for: .touchUpInside)
+    }
+    
+    @objc private func loginButtonPressed() {
+        //login button pressed
+        guard let emailText = loginView.emailTextField.text, !emailText.isEmpty else {
+            let errorAlert = Alert.createErrorAlert(withMessage: "You must enter a valid email address.", andCompletion: nil)
+            self.present(errorAlert, animated: true, completion: nil)
+            return
+        }
+        guard let passwordText = loginView.passwordTextField.text, !passwordText.isEmpty else {
+            let errorAlert = Alert.createErrorAlert(withMessage: "Please enter a password.", andCompletion: nil)
+            self.present(errorAlert, animated: true, completion: nil)
+            return
+        }
+        
+        AuthUserService.manager.delegate = self
+        AuthUserService.manager.signIn(withEmail: emailText, andPassword: passwordText) { (user, error) in
+            if let error = error {
+                let errorAlert = Alert.createErrorAlert(withMessage: error.localizedDescription, andCompletion: nil)
+                self.present(errorAlert, animated: true, completion: nil)
+            } else if let user = user {
+                //to do - should present the regular view controller
+                print(user)
+            }
+        }
+    }
+    
+    @objc private func createButtonPressed() {
+        let createAccountVC = CreateAccountVC()
+        self.present(createAccountVC, animated: true, completion: nil)
+    }
+}
+
+extension LoginVC: GIDSignInUIDelegate {
+    //what happens when you click the google sign in button
+    func sign(inWillDispatch signIn: GIDSignIn!, error: Error!) {
+        if let error = error {
+            print("error dispatching sign in: \(error)")
+        } else {
+            print("dispatched sign in")
+        }
+    }
+}
+
+extension LoginVC: AuthUserServiceDelegate {
+    func didSignInWithGoogle(_ authUserService: AuthUserService, user: User) {
+        print("did sign in with google")
+        //should launch the regular view controller
+    }
+    func didFailSignInWithGoogle(_ authUserService: AuthUserService, errorMessage: String) {
+        print("did fail sign in with google: \(errorMessage)")
+        let errorAlert = Alert.createErrorAlert(withMessage: "Could not sign in:\n\(errorMessage)", andCompletion: nil)
+        self.present(errorAlert, animated: true, completion: nil)
     }
 }
 
