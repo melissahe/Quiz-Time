b562e58a4672d0bbeab16943ec1ee9fe2918acdf
Melissa He <melissahe@ac.c4q.nyc>
2018-02-19 23:05:55 -0500

    Add flashcard and category model, with toJSON function, and custom sorting functions

---
+++ Quiz-Time/Models/Category.swift
@@ -0,0 +1,40 @@
+//
+//  Category.swift
+//  Quiz-Time
+//
+//  Created by C4Q on 2/19/18.
+//  Copyright © 2018 Melissa He @ C4Q. All rights reserved.
+//
+
+import Foundation
+
+struct Category: Codable {
+    let name: String
+    let userID: String
+    
+    init?(categoryDict: [String : Any]) {
+        guard let name = categoryDict["name"] as? String else {
+            print("couldn't get name")
+            return nil
+        }
+        guard let userID = categoryDict["userID"] as? String else {
+            print("couldn't get userID")
+            return nil
+        }
+        self.name = name
+        self.userID = userID
+    }
+    
+    func toJSON() -> Any {
+        let encoder = JSONEncoder()
+        let data = try! encoder.encode(self)
+        let json = try! JSONSerialization.jsonObject(with: data, options: [])
+        return json
+    }
+}
+
+extension Array where Element == Category {
+    func sortedAlphabetically() -> [Category] {
+        return self.sorted {$0.name.lowercased() < $1.name.lowercased()}
+    }
+}
---
+++ Quiz-Time/Models/Flashcard.swift
@@ -0,0 +1,61 @@
+//
+//  Flashcard.swift
+//  Quiz-Time
+//
+//  Created by C4Q on 2/19/18.
+//  Copyright © 2018 Melissa He @ C4Q. All rights reserved.
+//
+
+import Foundation
+
+struct Flashcard: Codable {
+    let question: String
+    let answer: String
+    let category: String
+    let userID: String
+    var timestamp: Double = Date.timeIntervalSinceReferenceDate
+    
+    init?(flashcardDict: [String : Any]) {
+        guard let question = flashcardDict["question"] as? String else {
+            print("couldn't get question")
+            return nil
+        }
+        guard let answer = flashcardDict["answer"] as? String else {
+            print("couldn't get answer")
+            return nil
+        }
+        guard let category = flashcardDict["category"] as? String else {
+            print("couldn't get category")
+            return nil
+        }
+        guard let userID = flashcardDict["userID"] as? String else {
+            print("couldn't get userID")
+            return nil
+        }
+        guard let timestamp = flashcardDict["timestamp"] as? Double else {
+            print("couldn't get timestamp")
+            return nil
+        }
+        self.question = question
+        self.answer = answer
+        self.category = category
+        self.userID = userID
+        self.timestamp = timestamp
+    }
+    
+    func toJSON() -> Any {
+        let encoder = JSONEncoder()
+        let data = try! encoder.encode(self)
+        let json = try! JSONSerialization.jsonObject(with: data, options: [])
+        return json
+    }
+}
+
+extension Array where Element == Flashcard {
+    func sortedByTimestamp() -> [Flashcard] {
+        return self.sorted {$0.timestamp < $1.timestamp}
+    }
+    func filterByCategory(categoryName: String) -> [Flashcard] {
+        return self.filter{$0.category == categoryName}
+    }
+}
