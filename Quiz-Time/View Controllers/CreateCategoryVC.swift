ab0feb4825938527bc3e119961f95a3ba1fb53d1
Melissa He <melissahe@ac.c4q.nyc>
2018-02-19 22:21:01 -0500

    Add more VCs

---
+++ Quiz-Time/View Controllers/CreateCategoryVC.swift
@@ -0,0 +1,17 @@
+//
+//  CreateCategoryVC.swift
+//  Quiz-Time
+//
+//  Created by C4Q on 2/19/18.
+//  Copyright © 2018 Melissa He @ C4Q. All rights reserved.
+//
+
+import UIKit
+
+class CreateCategoryVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
---
+++ Quiz-Time/View Controllers/HomeVC.swift
@@ -0,0 +1,17 @@
+//
+//  HomeVC.swift
+//  Quiz-Time
+//
+//  Created by C4Q on 2/19/18.
+//  Copyright © 2018 Melissa He @ C4Q. All rights reserved.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
