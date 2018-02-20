7276d8c7cfbbbdc11258f912f2f9265036329552
Melissa He <melissahe@ac.c4q.nyc>
2018-02-19 22:20:34 -0500

    Set up create account view and VC

--- Quiz-Time/CreateAccountView.swift
+++ Quiz-Time/CreateAccountView.swift
@@ -11,6 +11,85 @@ import SnapKit
 
 class CreateAccountView: UIView {
 
+    lazy var dismissButton: UIButton = {
+        let button = UIButton()
+        button.setImage(#imageLiteral(resourceName: "dismissButtonIcon"), for: .normal)
+        return button
+    }()
+    
+    lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Create Account"
+        label.font = UIFont(name: "Chalkduster", size: 48)
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        label.setContentHuggingPriority(UILayoutPriority(249), for: .vertical)
+        label.textColor = UIColor.lightPurple
+        return label
+    }()
+    
+    lazy var emailLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Email:"
+        label.textAlignment = .left
+        label.font = UIFont(name: "Chalkduster", size: 16)
+        label.textColor = UIColor.lightPurple
+        label.setContentHuggingPriority(UILayoutPriority(253), for: .horizontal)
+        label.setContentCompressionResistancePriority(UILayoutPriority(rawValue: 1000), for: .horizontal)
+        return label
+    }()
+    
+    lazy var emailTextField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .roundedRect
+        textField.placeholder = "Email..."
+        return textField
+    }()
+    
+    lazy var passwordLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Password:"
+        label.textAlignment = .left
+        label.font = UIFont(name: "Chalkduster", size: 16)
+        label.textColor = UIColor.lightPurple
+        label.setContentHuggingPriority(UILayoutPriority(253), for: .horizontal)
+        label.setContentCompressionResistancePriority(UILayoutPriority(rawValue: 1000), for: .horizontal)
+        return label
+    }()
+    
+    lazy var passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .roundedRect
+        textField.placeholder = "Password..."
+        textField.isSecureTextEntry = true
+        return textField
+    }()
+    
+    lazy var emailStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [emailLabel, emailTextField])
+        stackView.alignment = .fill
+        stackView.axis = .horizontal
+        stackView.spacing = 5
+        stackView.distribution = .fill
+        return stackView
+    }()
+    
+    lazy var passwordStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [passwordLabel, passwordTextField])
+        stackView.alignment = .fill
+        stackView.axis = .horizontal
+        stackView.spacing = 5
+        stackView.distribution = .fill
+        return stackView
+    }()
+    
+    lazy var createButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Create Account", for: .normal)
+        button.setTitleColor(UIColor.buttonBlue, for: .normal)
+        return button
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: UIScreen.main.bounds)
         commonInit()
@@ -27,7 +106,62 @@ class CreateAccountView: UIView {
     }
     
     private func setUpViews() {
-        
+        setUpDismissButton()
+        setUpTitleLabel()
+        setUpEmailStackView()
+        setUpPasswordStackView()
+        setUpCreateButton()
     }
-
+    
+    private func setUpDismissButton() {
+        self.addSubview(dismissButton)
+        
+        dismissButton.snp.makeConstraints { (make) in
+            make.leading.equalTo(self.snp.leading).offset(8)
+            make.top.equalTo(self.safeAreaLayoutGuide.snp.top).offset(8)
+        }
+    }
+    
+    private func setUpTitleLabel() {
+        self.addSubview(titleLabel)
+        
+        titleLabel.snp.makeConstraints { (make) in
+            make.top.equalTo(dismissButton).offset(80)
+            make.leading.trailing.equalTo(self).inset(80)
+        }
+    }
+    
+    private func setUpEmailStackView() {
+        self.addSubview(emailStackView)
+        
+        emailStackView.snp.makeConstraints { (make) in
+            make.top.equalTo(titleLabel.snp.bottom).offset(80)
+            make.leading.trailing.equalTo(self).inset(50)
+        }
+    }
+    
+    private func setUpPasswordStackView() {
+        self.addSubview(passwordStackView)
+        
+        passwordStackView.snp.makeConstraints { (make) in
+            make.top.equalTo(emailStackView.snp.bottom).offset(10)
+            make.width.equalTo(emailStackView.snp.width)
+            make.centerX.equalTo(emailStackView.snp.centerX)
+        }
+    }
+    
+    private func setUpCreateButton() {
+        self.addSubview(createButton)
+        
+        createButton.snp.makeConstraints { (make) in
+            make.top.equalTo(passwordStackView.snp.bottom).offset(40)
+            make.width.equalTo(self).multipliedBy(0.40)
+            make.centerX.equalTo(passwordStackView.snp.centerX)
+        }
+        
+        createButton.layer.borderColor = UIColor.lightPurple.cgColor
+        createButton.layer.borderWidth = 1.0
+        createButton.layer.masksToBounds = true
+        createButton.layer.cornerRadius = 10
+    }
 }
--- Quiz-Time/View Controllers/CreateAccountVC.swift
+++ Quiz-Time/View Controllers/CreateAccountVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SVProgressHUD
 
 class CreateAccountVC: UIViewController {
 
@@ -19,5 +20,41 @@ class CreateAccountVC: UIViewController {
     
     private func setUpViews() {
         self.view.addSubview(createAccountView)
+        createAccountView.dismissButton.addTarget(self, action: #selector(dismissButtonPressed), for: .touchUpInside)
+        createAccountView.createButton.addTarget(self, action: #selector(createButtonPressed), for: .touchUpInside)
+    }
+    
+    @objc private func dismissButtonPressed() {
+        self.dismiss(animated: true, completion: nil)
+    }
+    @objc private func createButtonPressed() {
+        guard let emailText = createAccountView.emailTextField.text, !emailText.isEmpty else {
+            let errorAlert = Alert.createErrorAlert(withMessage: "You must enter a valid email address.", andCompletion: nil)
+            self.present(errorAlert, animated: true, completion: nil)
+            return
+        }
+        guard let passwordText = createAccountView.passwordTextField.text, !passwordText.isEmpty else {
+            let errorAlert = Alert.createErrorAlert(withMessage: "Please enter a password.", andCompletion: nil)
+            self.present(errorAlert, animated: true, completion: nil)
+            return
+        }
+        SVProgressHUD.show()
+        AuthUserService.manager.createAccount(withEmail: emailText, andPassword: passwordText) { (user, error) in
+            SVProgressHUD.dismiss()
+            if let error = error {
+                let errorAlert = Alert.createErrorAlert(withMessage: "Could not create an account:\n\(error.localizedDescription)", andCompletion: nil)
+                self.present(errorAlert, animated: true, completion: nil)
+            } else if let user = user {
+                print(user)
+                //successful creating account
+                let successAlert = Alert.createAlert(withTitle: "Success!!", andMessage: "You created an account!!")
+                Alert.addAction(withTitle: "Lol thanks 😂", style: .default, andCompletion: { (_) in
+                    //present tab bar
+                    let tabBarVC = TabBarController()
+                    self.present(tabBarVC, animated: true, completion: nil)
+                }, toAlertController: successAlert)
+                self.present(successAlert, animated: true, completion: nil)
+            }
+        }
     }
 }
