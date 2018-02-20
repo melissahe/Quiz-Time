1d397edc314f63977e1cd9e428330f534bc1befa
Melissa He <melissahe@ac.c4q.nyc>
2018-02-20 00:13:34 -0500

    Add empty views

---
+++ Quiz-Time/CreateFlashCardView.swift
@@ -0,0 +1,31 @@
+//
+//  CreateFlashCardView.swift
+//  Quiz-Time
+//
+//  Created by C4Q on 2/19/18.
+//  Copyright © 2018 Melissa He @ C4Q. All rights reserved.
+//
+
+import UIKit
+
+class CreateFlashCardView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: UIScreen.main.bounds)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        backgroundColor = .white
+        setUpViews()
+    }
+
+    private func setUpViews() {
+        
+    }
+}
