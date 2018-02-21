58ef3bcf03f736908b8d06f5736fe34dfec55fab
Melissa He <melissahe@ac.c4q.nyc>
2018-02-20 19:03:40 -0500

    Add create flash card view

--- Quiz-Time/CreateFlashCardView.swift
+++ Quiz-Time/CreateFlashCardView.swift
@@ -8,8 +8,58 @@
 
 import UIKit
 
-class CreateFlashCardView: UIView {
+class CreateFlashCardView: UIScrollView {
 
+    lazy var contentView: UIView = {
+        let view = UIView()
+        return view
+    }()
+    
+    lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Add A Flashcard"
+        label.font = UIFont(name: "Chalkduster", size: 32)
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        label.setContentHuggingPriority(UILayoutPriority(249), for: .vertical)
+        label.textColor = UIColor.lightPurple
+        return label
+    }()
+    
+    lazy var questionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Question:"
+        label.textColor = .lightPurple
+        label.font = UIFont(name: "Chalkduster", size: 20)
+        label.setContentHuggingPriority(UILayoutPriority(252), for: .horizontal)
+        label.setContentCompressionResistancePriority(UILayoutPriority(1000), for: .horizontal)
+        return label
+    }()
+    
+    lazy var questionTextField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .bezel
+        textField.placeholder = "Question here..."
+        textField.isUserInteractionEnabled = true
+        textField.setContentHuggingPriority(UILayoutPriority(252), for: .horizontal)
+        return textField
+    }()
+    
+    lazy var answerLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Answer:"
+        label.textColor = .lightPurple
+        label.font = UIFont(name: "Chalkduster", size: 20)
+        label.setContentHuggingPriority(UILayoutPriority(252), for: .horizontal)
+        label.setContentCompressionResistancePriority(UILayoutPriority(1000), for: .horizontal)
+        return label
+    }()
+    
+    lazy var answerTextView: UITextView = {
+        let textView = UITextView()
+        return textView
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: UIScreen.main.bounds)
         commonInit()
@@ -26,6 +76,73 @@ class CreateFlashCardView: UIView {
     }
 
     private func setUpViews() {
+        setUpContentView()
+        setUpTitleLabel()
+        setUpQuestionLabel()
+        setUpQuestionTextField()
+        setUpAnswerLabel()
+        setUpAnswerTextView()
+    }
+    
+    private func setUpContentView() {
+        self.addSubview(contentView)
+        contentView.snp.makeConstraints { (make) in
+            make.edges.equalTo(self)
+            make.width.equalTo(self.snp.width)
+            make.center.equalTo(self.snp.center)
+        }
+    }
+    
+    private func setUpTitleLabel() {
+        contentView.addSubview(titleLabel)
         
+        titleLabel.snp.makeConstraints { (make) in
+            make.top.equalTo(contentView.safeAreaLayoutGuide.snp.top).offset(40)
+            make.leading.trailing.equalTo(contentView).inset(40)
+        }
+    }
+    
+    private func setUpQuestionLabel() {
+        contentView.addSubview(questionLabel)
+        
+        questionLabel.snp.makeConstraints { (make) in
+            make.top.equalTo(titleLabel.snp.bottom).offset(40)
+            make.leading.equalTo(contentView).offset(20)
+        }
+    }
+    
+    private func setUpQuestionTextField() {
+        contentView.addSubview(questionTextField)
+        
+        questionTextField.snp.makeConstraints { (make) in
+            make.leading.equalTo(questionLabel.snp.trailing).offset(5)
+            make.trailing.equalTo(contentView).offset(-20)
+            make.height.centerY.equalTo(questionLabel)
+        }
+    }
+    
+    private func setUpAnswerLabel() {
+        contentView.addSubview(answerLabel)
+        
+        answerLabel.snp.makeConstraints { (make) in
+            make.top.equalTo(questionLabel.snp.bottom).offset(10)
+            make.leading.equalTo(questionLabel.snp.leading)
+        }
+    }
+    
+    private func setUpAnswerTextView() {
+        contentView.addSubview(answerTextView)
+        
+        answerTextView.snp.makeConstraints { (make) in
+            make.top.equalTo(answerLabel.snp.bottom).offset(10)
+            make.leading.trailing.equalTo(contentView).inset(20)
+            make.height.lessThanOrEqualTo(contentView).multipliedBy(0.40)
+            make.bottom.equalTo(contentView).offset(-40)
+        }
+        
+        answerTextView.layer.masksToBounds = true
+        answerTextView.layer.cornerRadius = 10
+        answerTextView.layer.borderWidth = 1
+        answerTextView.layer.borderColor = UIColor.lightPurple.cgColor
     }
 }
--- Quiz-Time/View Controllers/CreateFlashCardVC.swift
+++ Quiz-Time/View Controllers/CreateFlashCardVC.swift
@@ -16,14 +16,35 @@ class CreateFlashCardVC: UIViewController {
         super.viewDidLoad()
         self.navigationItem.title = "Create Flashcard"
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addCardButtonPressed))
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .trash, target: self, action: #selector(resetButtonPressed))
         setUpViews()
+        setUpGestures()
     }
     
     private func setUpViews() {
         self.view.addSubview(createFlashCardView)
+        
+        createFlashCardView.snp.makeConstraints { (make) in
+            make.edges.equalTo(self.view)
+            make.center.equalTo(self.view.snp.center)
+        }
+    }
+    
+    private func setUpGestures() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(scrollViewTapped))
+        
+        createFlashCardView.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc private func scrollViewTapped() {
+        self.view.endEditing(true)
     }
     
     @objc private func addCardButtonPressed() {
+        //to do
+    }
+    
+    @objc private func resetButtonPressed() {
         //to do
     }
 }
