1264bafcfdadeb77474df690d426a6b534cfc707
Melissa He <melissahe@ac.c4q.nyc>
2018-02-21 19:40:25 -0500

    Set up CreateCategoryVC

--- Quiz-Time/View Controllers/CreateCategoryVC.swift
+++ Quiz-Time/View Controllers/CreateCategoryVC.swift
@@ -12,12 +12,60 @@ class CreateCategoryVC: UIViewController {
     
     private let createCategoryView = CreateCategoryView()
     
+    private var databaseService = DatabaseService()
+    private var currentUserID: String {
+        return AuthUserService.manager.getCurrentUser()!.uid
+    }
+    
+    private var categories: [Category] {
+        didSet {
+            //to do - reload table view
+        }
+    }
+    
+    init(categories: [Category]) {
+        if categories.isEmpty {
+            databaseService.getAllCategories(fromUserID: currentUserID, completion: { [weak self] (categories) in
+                if let categories = categories {
+                    self?.categories = categories
+                } else {
+                    let errorAlert = Alert.createErrorAlert(withMessage: "Couldn't load the categories!!", andCompletion: nil)
+                    self?.present(errorAlert, animated: true, completion: nil)
+                }
+            })
+        } else {
+            self.categories = categories
+        }
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpViews()
+        setUpNavigation()
     }
     
     private func setUpViews() {
         self.view.addSubview(createCategoryView)
+    }
+    
+    private func setUpNavigation() {
+        self.navigationController?.navigationBar.barTintColor = .lightPurple
+        self.navigationController?.navigationBar.isTranslucent = false
+        
+        self.navigationItem.title = "Add A Category"
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addCategoryButtonPressed))
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(cancelButtonPressed))
+    }
+    
+    @objc private func addCategoryButtonPressed() {
+        
+    }
+    @objc private func cancelButtonPressed() {
+        self.navigationController?.dismiss(animated: true, completion: nil)
     }
 }
