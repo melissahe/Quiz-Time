2d6f9afc58720d7e0e0e9fb9223913d5f6c867bc
Melissa He <melissahe@ac.c4q.nyc>
2018-02-21 20:25:35 -0500

    Set up CreateCategoryView

--- Quiz-Time/CreateCategoryView.swift
+++ Quiz-Time/CreateCategoryView.swift
@@ -10,6 +10,22 @@ import UIKit
 
 class CreateCategoryView: UIView {
 
+    lazy var categoryLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Category:"
+        label.textColor = .lightPurple
+        label.font = UIFont(name: "Chalkduster", size: 20)
+        label.setContentHuggingPriority(UILayoutPriority(252), for: .horizontal)
+        label.setContentCompressionResistancePriority(UILayoutPriority(1000), for: .horizontal)
+        return label
+    }()
+    
+    lazy var categoryTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Category..."
+        textField.borderStyle = .bezel
+        return textField
+    }()
     
     override init(frame: CGRect) {
         super.init(frame: UIScreen.main.bounds)
@@ -27,6 +43,24 @@ class CreateCategoryView: UIView {
     }
     
     private func setUpViews() {
-        
+        setUpCategoryLabel()
+        setUpCategoryTextField()
+    }
+    
+    private func setUpCategoryLabel() {
+        self.addSubview(categoryLabel)
+        categoryLabel.snp.makeConstraints { (make) in
+            make.top.equalTo(self.safeAreaLayoutGuide.snp.top).offset(20)
+            make.leading.equalTo(self).offset(20)
+        }
+    }
+    
+    private func setUpCategoryTextField() {
+        self.addSubview(categoryTextField)
+        categoryTextField.snp.makeConstraints { (make) in
+            make.leading.equalTo(categoryLabel.snp.trailing).offset(5)
+            make.trailing.equalTo(self).offset(-20)
+            make.height.centerY.equalTo(categoryLabel)
+        }
     }
 }
